f0561df9d2f30c3a09a6aa27241bc669ce72da01
weekend27 <weekend27@163.com>
2015-11-30 22:15:48 +0800

    NumberClass

---
+++ NumberClass/Demo1.java
@@ -0,0 +1,9 @@
+//import java.lang.Integer;
+
+public class Demo1{
+  public static void main(String args[]){
+    Integer x = 5;  // boxes int to an Integer object
+    x = x + 12;     // unboxes the Integer to a int
+    System.out.println(x);
+  }
+}
