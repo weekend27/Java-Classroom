9b1efc438b27dc47ba6ca62db5d1d60e0b56176a
weekend27 <weekend27@163.com>
2015-12-01 11:28:09 +0800

    Re

---
+++ Re/Demo1.java
@@ -0,0 +1,27 @@
+/*
+捕获组是把多个字符当一个单独单元进行处理的方法，它通过对括号内的字符分组来创建。
+*/
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Demo1{
+  public static void main(String[] args){
+    // 按指定模式在字符串查找
+    String line = "This order was placed for QT3000! OK?";
+    String pattern = "(.*)(\\d+)(.*)";
+
+    // 创建 Pattern 对象
+    Pattern r = Pattern.compile(pattern);
+
+    // 现在创建 matcher 对象
+    Matcher m = r.matcher(line);
+    if (m.find()){
+      System.out.println("Found value: " + m.group(0));
+      System.out.println("Found value: " + m.group(1));
+      System.out.println("Found value: " + m.group(2));
+    }else {
+      System.out.println("No Match!");
+    }
+  }
+}
